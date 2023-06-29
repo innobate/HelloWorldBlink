c081216d45c0966220a9130a647b66348a2856fc
Shelvin Datt <shelvindatt@gmail.com>
2023-06-29 21:57:35 +1200

    Format code

--- hello_world_blink.c
+++ hello_world_blink.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-    #include "pico/stdlib.h"
-    #include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
-    int main() {
+int main()
+{
 
     stdio_init_all();
-     if (cyw43_arch_init()) {
-       printf("Wi-Fi init failed");
+    if (cyw43_arch_init())
+    {
+        printf("Wi-Fi init failed");
         return -1;
-     }
+    }
 
-    const uint LED_PIN =  CYW43_WL_GPIO_LED_PIN;
+    const uint LED_PIN = CYW43_WL_GPIO_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
+    while (true)
+    {
         cyw43_arch_gpio_put(LED_PIN, 1);
         sleep_ms(250);
         cyw43_arch_gpio_put(LED_PIN, 0);
@@ -21,4 +24,4 @@
         printf("Hello, world!\n I am blinking!\n");
         sleep_ms(500);
     }
-    }
+}
